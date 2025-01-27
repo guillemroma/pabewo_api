cfea49e2e00df07896621ccb4f28f320f1634d61
guillemroma <guillemroma.lopez@gmail.com>
2025-01-27 21:31:21 +0100

    Enhances controller

--- app/controllers/api/v1/telegram_controller.rb
+++ app/controllers/api/v1/telegram_controller.rb
@@ -5,29 +5,22 @@ module Api
     class TelegramController < ApplicationController
       def webhook
         token = ENV.fetch('TELEGRAM_BOT_TOKEN')
+        client = Telegram::Bot::Client.new(ENV.fetch('TELEGRAM_BOT_TOKEN'))
 
-        Telegram::Bot::Client.run(token) do |bot|
-          bot.listen do |message|
-            chat_id = message.chat.id
-            message_thread_id = message.message_thread_id
-            user_name = message.from.first_name
-        
-            response_text = "Hello, #{user_name}! This is a reply."
-        
-            if thread_id
-              bot.api.send_message(
-                chat_id: chat_id,
-                text: response_text,
-                message_thread_id: thread_id
-              )
-            else
-              bot.api.send_message(
-                chat_id: chat_id,
-                text: response_text
-              )
-            end
-          end
+        chat_id = params.dig("message", "chat", "id")
+        message_thread_id = params.dig("message", "message_thread_id")
+
+        if message_thread_id.present?
+          reply_message(client, chat_id, message_thread_id, "This is a reply in the topic.")
+        else
+          reply_message(client, chat_id, "This is a reply in the private chat.")
         end
+
+        render json: { status: "success" }, status: :ok                
+      end
+
+      def reply_message(client, chat_id, message_thread_id, message)
+        client.api.send_message(chat_id: chat_id, text: message, message_thread_id: message_thread_id)
       end
     end
   end
