d09f2c69d7df7593e42a71ac0e51c7dc0c0f4cc0
guillemroma <guillemroma.lopez@gmail.com>
2025-01-27 21:34:48 +0100

    Enhance controller

--- app/controllers/api/v1/telegram_controller.rb
+++ app/controllers/api/v1/telegram_controller.rb
@@ -7,13 +7,18 @@ module Api
         token = ENV.fetch('TELEGRAM_BOT_TOKEN')
         client = Telegram::Bot::Client.new(ENV.fetch('TELEGRAM_BOT_TOKEN'))
 
+        Rails.logger.info("params: #{params}")
+
         chat_id = params.dig("message", "chat", "id")
         message_thread_id = params.dig("message", "message_thread_id")
+
+        Rails.logger.info("chat_id: #{chat_id}")
+        Rails.logger.info("message_thread_id: #{message_thread_id}")
 
         if message_thread_id.present?
           reply_message(client, chat_id, message_thread_id, "This is a reply in the topic.")
         else
-          reply_message(client, chat_id, "This is a reply in the private chat.")
+          reply_message(client, chat_id, nil, "This is a reply in the private chat.")
         end
 
         render json: { status: "success" }, status: :ok                
