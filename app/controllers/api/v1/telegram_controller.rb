842d0531a6a0bc845f710d4702a0e8465889f802
guillemroma <guillemroma.lopez@gmail.com>
2025-01-27 21:47:05 +0100

    EC

--- app/controllers/api/v1/telegram_controller.rb
+++ app/controllers/api/v1/telegram_controller.rb
@@ -10,15 +10,16 @@ module Api
         Rails.logger.info("params: #{params}")
 
         chat_id = params.dig("message", "chat", "id")
+        message = params.dig("message", "text")
         message_thread_id = params.dig("message", "message_thread_id")
 
         Rails.logger.info("chat_id: #{chat_id}")
         Rails.logger.info("message_thread_id: #{message_thread_id}")
 
         if message_thread_id.present?
-          reply_message(client, chat_id, message_thread_id, "This is a reply in the topic.")
+          reply_message(client, chat_id, message_thread_id, "You said: #{message}, I say: This is a reply in the topic.")
         else
-          reply_message(client, chat_id, nil, "This is a reply in the private chat.")
+          reply_message(client, chat_id, nil, "You said: #{message}, I say: This is a reply in the private chat.")
         end
 
         render json: { status: "success" }, status: :ok                
